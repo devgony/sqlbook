11b132a836b0a2b7fcd98dde4f5ef4894732b300
devgony <devgony@gmail.com>
2022-02-15 00:01:08 +0900

    [client]tailwind setup

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="bg-green-500">
+      <h1 className="text-blue-400">it works</h1>
+      <button className="btn">Test</button>
+    </div>
+  );
 };
 
 export default Home;
