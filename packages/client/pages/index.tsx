59c7ab97c50113443cd858de9b83e9638c945da3
devgony <devgony@gmail.com>
2022-02-15 13:17:27 +0900

    [client]Init graphql

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,6 +1,11 @@
-import type { NextPage } from 'next';
+import { gql } from '@apollo/client';
+import type { InferGetServerSidePropsType, NextPage } from 'next';
+import client from '../apollo-client';
 
-const Home: NextPage = () => {
+const Home = ({
+  countries,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(countries);
   return (
     <div className="bg-green-500">
       <h1 className="text-blue-400">it works</h1>
@@ -8,5 +13,25 @@ const Home: NextPage = () => {
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const { data } = await client.query({
+    query: gql`
+      query Countries {
+        countries {
+          code
+          name
+          emoji
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      countries: data.countries.slice(0, 4),
+    },
+  };
+}
 
 export default Home;
