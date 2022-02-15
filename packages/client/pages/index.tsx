32b7362c8f20402fce7e2d6eee03f222f6081579
devgony <devgony@gmail.com>
2022-02-15 15:36:53 +0900

    [client]graphql-codegen setup with --watch

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -3,9 +3,9 @@ import type { InferGetServerSidePropsType, NextPage } from 'next';
 import client from '../apollo-client';
 
 const Home = ({
-  countries,
+  data,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  console.log(countries);
+  console.log(data);
   return (
     <div className="bg-green-500">
       <h1 className="text-blue-400">it works</h1>
@@ -17,11 +17,13 @@ const Home = ({
 export async function getServerSideProps() {
   const { data } = await client.query({
     query: gql`
-      query Countries {
-        countries {
-          code
-          name
-          emoji
+      query findDbs {
+        findDbs {
+          dbs {
+            id
+            password
+            name
+          }
         }
       }
     `,
@@ -29,7 +31,7 @@ export async function getServerSideProps() {
 
   return {
     props: {
-      countries: data.countries.slice(0, 4),
+      data,
     },
   };
 }
