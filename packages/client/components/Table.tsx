9803f7cd69d29a46770023bd74eccee5cdaa63f7
devgony <devgony@gmail.com>
2022-03-30 00:14:55 +0900

    [client]Add useResizeColumns, SSR for styledComponent

--- packages/client/components/Table.tsx
+++ packages/client/components/Table.tsx
@@ -1,55 +1,241 @@
-import { useState } from 'react';
-import { usePagination, useSortBy, useTable } from 'react-table';
+import React, { forwardRef, useEffect, useMemo, useRef } from 'react';
+import styled from 'styled-components';
+import {
+  useTable,
+  useResizeColumns,
+  useFlexLayout,
+  useRowSelect,
+  useSortBy,
+  UseTableColumnProps,
+} from 'react-table';
+
+const Styles = styled.div`
+  padding: 1rem;
+  ${
+    '' /* These styles are suggested for the table fill all available space in its containing element */
+  }
+  display: block;
+  ${
+    '' /* These styles are required for a horizontaly scrollable table overflow */
+  }
+  overflow: auto;
+
+  .table {
+    border-spacing: 0;
+    border: 1px solid #ddd;
+    /* font-family: Arial, Helvetica, sans-serif; */
+    /* border-collapse: collapse; */
+    font-size: 12px;
+    .thead {
+      ${
+        '' /* These styles are required for a scrollable body to align with the header properly */
+      }
+      overflow-y: auto;
+      overflow-x: hidden;
+      background-color: #04aa6d;
+      color: white;
+    }
+
+    .tr:nth-child(even) {
+      background-color: #f2f2f2;
+    }
+
+    .tr:hover {
+      background-color: #ddd;
+    }
+
+    .tbody {
+      ${'' /* These styles are required for a scrollable table body */}
+      overflow-y: scroll;
+      overflow-x: hidden;
+      /* height: 250px; */
+    }
+
+    .tr {
+      :last-child {
+        .td {
+          border-bottom: 0;
+        }
+      }
+      border-bottom: 1px solid #ddd;
+    }
+
+    .th,
+    .td {
+      margin: 0;
+      /* padding: 0.5rem; */
+      padding: 0.5rem 0.1rem;
+      border-right: 1px solid #ddd;
+
+      ${
+        '' /* In this example we use an absolutely position resizer,
+       so this is required. */
+      }
+      position: relative;
+
+      :last-child {
+        border-right: 0;
+      }
+
+      .resizer {
+        right: 0;
+        /* background: gray; */
+        width: 10px;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        z-index: 1;
+        ${'' /* prevents from scrolling while dragging on touch devices */}
+        touch-action :none;
+
+        &.isResizing {
+          background: red;
+        }
+      }
+    }
+  }
+`;
+
+//@ts-ignore
+// const headerProps = (props, { column }) => getStyles(props, column.align);
+//@ts-ignore
+const cellProps = (props, { cell }) => getStyles(props, cell.column.align);
+//@ts-ignore
+const getStyles = (props, align = 'left') => [
+  props,
+  {
+    style: {
+      justifyContent: align === 'right' ? 'flex-end' : 'flex-start',
+      alignItems: 'flex-start',
+      display: 'flex',
+    },
+  },
+];
+
+const IndeterminateCheckbox = forwardRef(
+  ({ indeterminate, ...rest }: any, ref) => {
+    const defaultRef = useRef();
+    const resolvedRef = ref || defaultRef;
+
+    useEffect(() => {
+      // @ts-ignore
+      resolvedRef.current.indeterminate = indeterminate;
+    }, [resolvedRef, indeterminate]);
+
+    return (
+      <>
+        <input type="checkbox" ref={resolvedRef} {...rest} />
+      </>
+    );
+  },
+);
 
 export default function Table({ columns, data }: any) {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable(
-      {
-        data,
-        columns,
-      },
-      useSortBy,
-    );
+  const defaultColumn = useMemo(
+    () => ({
+      // When using the useFlexLayout:
+      minWidth: 30, // minWidth is only used as a limit for resizing
+      width: 150, // width is used for both the flex-basis and flex-grow
+      maxWidth: 200, // maxWidth is only used as a limit for resizing
+    }),
+    [],
+  );
+
+  const { getTableProps, headerGroups, rows, prepareRow } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
+    },
+    useResizeColumns,
+    useFlexLayout,
+    // useRowSelect,
+    useSortBy,
+    // hooks => {
+    //   hooks.allColumns.push(columns => [
+    //     // Let's make a column for selection
+    //     {
+    //       id: 'selection',
+    //       disableResizing: true,
+    //       minWidth: 35,
+    //       width: 35,
+    //       maxWidth: 35,
+    //       // The header can use the table's getToggleAllRowsSelectedProps method
+    //       // to render a checkbox
+    //       Header: ({ getToggleAllRowsSelectedProps }) => (
+    //         <div>
+    //           <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
+    //         </div>
+    //       ),
+    //       // The cell can use the individual row's getToggleRowSelectedProps method
+    //       // to the render a checkbox
+    //       Cell: ({ row }) => (
+    //         <div>
+    //           <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
+    //         </div>
+    //       ),
+    //     },
+    //     ...columns,
+    //   ]);
+    //   hooks.useInstanceBeforeDimensions.push(({ headerGroups }) => {
+    //     // fix the parent group of the selection button to not be resizable
+    //     const selectionGroupHeader = headerGroups[0].headers[0];
+    //     selectionGroupHeader.canResize = false;
+    //   });
+    // },
+  );
+
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th
-                id="arrow-container"
-                {...column.getHeaderProps(column.getSortByToggleProps())}
-              >
-                <span id="arrow">
-                  {column.isSortedDesc ? '⬇' : '⬆'}
-                  {/* {column.isSorted ? (
-                    column.isSortedDesc ? (
-                      ' ⬇️'
-                    ) : (
-                      ' ⬆️'
-                    )
-                  ) : (
-                    <i id="arrow"> ⬇️</i>
-                  )} */}
-                </span>
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+    <Styles>
+      <div {...getTableProps()} className="table">
+        <div>
+          {headerGroups.map(headerGroup => (
+            <div
+              {...headerGroup.getHeaderGroupProps({
+                // style: { paddingRight: '15px' },
               })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+              className="tr"
+            >
+              {headerGroup.headers.map(column => (
+                <div
+                  className="th thead"
+                  id="arrow-container"
+                  // {...column.getHeaderProps(headerProps)}
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                >
+                  <span id="arrow">{column.isSortedDesc ? '⬇' : '⬆'}</span>
+                  {column.render('Header')}
+                  {/* Use column.getResizerProps to hook up the events correctly */}
+                  {column.canResize && (
+                    <div
+                      {...column.getResizerProps()}
+                      className={`resizer ${
+                        column.isResizing ? 'isResizing' : ''
+                      }`}
+                    />
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+        <div className="tbody">
+          {rows.map(row => {
+            prepareRow(row);
+            return (
+              <div {...row.getRowProps()} className="tr">
+                {row.cells.map(cell => {
+                  return (
+                    <div {...cell.getCellProps(cellProps)} className="td">
+                      {cell.render('Cell')}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </Styles>
   );
 }
--- packages/client/pages/top-sql.tsx
+++ packages/client/pages/top-sql.tsx
@@ -1,4 +1,4 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useLazyQuery, useQuery } from '@apollo/client';
 import { NextPage } from 'next';
 import { useMemo } from 'react';
 import Table from '../components/Table';
@@ -6,6 +6,46 @@ import {
   FindSqlHistsQuery,
   FindSqlHistsQueryVariables,
 } from '../generated/graphql';
+
+const headers = [
+  'SNAP_ID',
+  'BEGIN_INTERVAL_TIME',
+  'END_INTERVAL_TIME',
+  'DBID',
+  'INSTANCE_NUMBER',
+  'SQL_ID',
+  'PLAN_HASH_VALUE',
+  'VERSION_COUNT',
+  'MODULE',
+  'FETCHES_TOTAL',
+  'FETCHES_DELTA',
+  'END_OF_FETCH_COUNT_TOTAL',
+  'END_OF_FETCH_COUNT_DELTA',
+  'SORTS_TOTAL',
+  'SORTS_DELTA',
+  'EXECUTIONS_TOTAL',
+  'EXECUTIONS_DELTA',
+  'LOADS_TOTAL',
+  'LOADS_DELTA',
+  'PARSE_CALLS_TOTAL',
+  'PARSE_CALLS_DELTA',
+  'DISK_READS_TOTAL',
+  'DISK_READS_DELTA',
+  'BUFFER_GETS_TOTAL',
+  'BUFFER_GETS_DELTA',
+  'ROWS_PROCESSED_TOTAL',
+  'ROWS_PROCESSED_DELTA',
+  'CPU_TIME_TOTAL',
+  'CPU_TIME_DELTA',
+  'ELAPSED_TIME_TOTAL',
+  'ELAPSED_TIME_DELTA',
+  'DIRECT_WRITES_TOTAL',
+  'DIRECT_WRITES_DELTA',
+  'PHYSICAL_READ_BYTES_TOTAL',
+  'PHYSICAL_READ_BYTES_DELTA',
+  'PHYSICAL_WRITE_BYTES_TOTAL',
+  'PHYSICAL_WRITE_BYTES_DELTA',
+];
 
 const FIND_SQL_HISTS = gql`
   query findSqlHists($input: FindSqlHistsInput!) {
@@ -58,61 +98,30 @@ const FIND_SQL_HISTS = gql`
 `;
 
 const TopSql: NextPage = () => {
-  const { data, error } = useQuery<
+  const [findSqlHists, { data, error }] = useLazyQuery<
     FindSqlHistsQuery,
     FindSqlHistsQueryVariables
   >(FIND_SQL_HISTS, { variables: { input: { page: 1 } } });
   const columns = useMemo(
-    () =>
-      [
-        'SNAP_ID',
-        'BEGIN_INTERVAL_TIME',
-        'END_INTERVAL_TIME',
-        'DBID',
-        'INSTANCE_NUMBER',
-        'SQL_ID',
-        'PLAN_HASH_VALUE',
-        'VERSION_COUNT',
-        'MODULE',
-        'FETCHES_TOTAL',
-        'FETCHES_DELTA',
-        'END_OF_FETCH_COUNT_TOTAL',
-        'END_OF_FETCH_COUNT_DELTA',
-        'SORTS_TOTAL',
-        'SORTS_DELTA',
-        'EXECUTIONS_TOTAL',
-        'EXECUTIONS_DELTA',
-        'LOADS_TOTAL',
-        'LOADS_DELTA',
-        'PARSE_CALLS_TOTAL',
-        'PARSE_CALLS_DELTA',
-        'DISK_READS_TOTAL',
-        'DISK_READS_DELTA',
-        'BUFFER_GETS_TOTAL',
-        'BUFFER_GETS_DELTA',
-        'ROWS_PROCESSED_TOTAL',
-        'ROWS_PROCESSED_DELTA',
-        'CPU_TIME_TOTAL',
-        'CPU_TIME_DELTA',
-        'ELAPSED_TIME_TOTAL',
-        'ELAPSED_TIME_DELTA',
-        'DIRECT_WRITES_TOTAL',
-        'DIRECT_WRITES_DELTA',
-        'PHYSICAL_READ_BYTES_TOTAL',
-        'PHYSICAL_READ_BYTES_DELTA',
-        'PHYSICAL_WRITE_BYTES_TOTAL',
-        'PHYSICAL_WRITE_BYTES_DELTA',
-      ].map(v => ({ Header: v, accessor: v })),
+    () => headers.map(v => ({ Header: v, accessor: v })),
     [],
   );
-
   return (
     <div className="mt-10">
       <h1>TOP SQL List</h1>
-      <input placeholder="search" />
-      <div className="">
-        {data?.findSqlHists.sqlHists && (
+      <input className="bg-gray-200 mr-1" placeholder="DB" />
+      <input className="bg-gray-200 mr-1" placeholder="Date" />
+      <input className="bg-gray-200 mr-1" placeholder="Time" />
+      <input className="bg-gray-200 mr-1" placeholder="Module" />
+      <input className="bg-gray-200 mr-1" placeholder="User" />
+      <button className="btn" onClick={() => findSqlHists()}>
+        Search
+      </button>
+      <div>
+        {data?.findSqlHists.sqlHists ? (
           <Table columns={columns} data={data.findSqlHists.sqlHists} />
+        ) : (
+          <Table columns={columns} data={[]} />
         )}
       </div>
     </div>
